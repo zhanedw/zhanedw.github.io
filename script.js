6f1f1fda36dd4377a036cfe01c2213954d74bfcd
zhanedw <haiting.hz.zhang@gmail.com>
2022-02-10 20:20:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ var mapSpec = {
     "stroke": "white"
   }
 };
+
+var map2Spec = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "width": 500,
+  "height": 300,
+  "data": {
+    "url": "data/ca-merge-topo.json",
+    "format": {
+      "type": "topojson",
+      "feature": "counties"
+    }
+  },
+  "projection": {
+    "type": "albersUsa"
+  },
+  "mark": {
+    "type": "geoshape",
+    "fill": "blue",
+    "stroke": "white"
+  }
+};
