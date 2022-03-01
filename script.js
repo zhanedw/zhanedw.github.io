5b375ba54f674c49668fc4ae8760180aaef8b121
zhanedw <haiting.hz.zhang@gmail.com>
2022-03-01 10:02:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
-var mapSpec = {
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "width": 500,
-  "height": 300,
-  "data": {
-    "url": "data/us-10m.json",
-    "format": {
-      "type": "topojson",
-      "feature": "counties"
-    }
+// setup API options
+const options = {
+  config: {
+    // Vega-Lite default configuration
   },
-  "projection": {
-    "type": "albersUsa"
+  init: (view) => {
+    // initialize tooltip handler
+    view.tooltip(new vegaTooltip.Handler().call);
   },
-  "mark": {
-    "type": "geoshape",
-    "fill": "blue",
-    "stroke": "white"
-  }
+  view: {
+    // view constructor options
+    // remove the loader if you don't want to default to vega-datasets!
+    // loader: vega.loader({
+    //   baseURL: "https://cdn.jsdelivr.net/npm/vega-datasets@2/",
+    // }),
+    renderer: "canvas",
+  },
 };
 
-var map2Spec = {
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "width": 500,
-  "height": 300,
-  "data": {
-    "url": "data/ca-merge-topo.json",
-    "format": {
-      "type": "topojson",
-      "feature": "counties"
-    }
-  },
-  "projection": {
-   "type": "identity",
-    "reflectY": true
-  },
-  "mark": {
-    "type": "geoshape",
-    "fill": "blue",
-    "stroke": "white"
-  }
-};
+// register vega and vega-lite with the API
+vl.register(vega, vegaLite, options);
+
+// Population Bar
+vl.markBar({ tooltip: true })
+  .data('data/provinces_code_pop.csv')
+  .encode(
+    vl.x().fieldQ('Population, 2021'),
+    vl.y().fieldN("Geographic name"),
+    vl.tooltip([vl.fieldQ("Population, 2021"), vl.fieldN("Geographic name")])
+  )
+  .render()
+  .then(viewElement => {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('populationBar').appendChild(viewElement);
+  });
+
+// Population Pie
+vl.markArc({ tooltip: true })
+  .data('data/provinces_code_pop.csv')
+  .encode(
+    vl.theta().fieldQ('Population, 2021'),
+    vl.color().fieldN("Geographic name"),
+    vl.tooltip([vl.fieldQ("Population, 2021"), vl.fieldN("Geographic name")])
+  )
+  .render()
+  .then(viewElement => {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('populationPie').appendChild(viewElement);
+  });
+
+// Canada population Choropleth Map
+vl.markGeoshape({stroke: '#aaa', strokeWidth: 0.25})
+  .data(vl.topojson('data/lpr_000b21a_e_topo.json').feature('lpr_000b21a_e'))
+  .transform(
+    vl.lookup('properties.DGUID')
+      .from(vl.data('data/provinces_code_pop.csv').key('DGUID').fields(['Population, 2021']))
+  )
+  .encode(
+    vl.color().fieldQ('Population, 2021'),
+    vl.tooltip().fieldQ('Population, 2021')
+  )
+  .project(vl.projection('identity').reflectY(true))
+  .width(500).height(300)
+  .config({view: {stroke: null}})
+  .render()
+  .then(viewElement => {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('populationMap').appendChild(viewElement);
+  });
